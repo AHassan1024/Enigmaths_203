2d719f923f7135b28690f8c9c220ccc61c0e8de1
Ameena Hassan <ameenahassan1024@gmail.com>
2022-12-07 23:58:26 +0000

    Identifying cube numbers

--- mt_parser.py
+++ mt_parser.py
@@ -7,12 +7,15 @@ print("Welcome to the [1..49] parser!")
 for n in range(1, 50):
     a = int(n // 10)
     b = n % 10
-    print(n)
+    # print(n)
 
     # Creating a flag to check if a number is 'special'
     special = False
 
     # If n is a cube, set special as True
+    if (int(round(n ** (1. / 3)) ** 3) == n):
+        special = True
+        print("Cube: ", n)
     # If n is a square, set special as True
     # If n is a prime, set special as True
     # If n is a triangular, set special as True
